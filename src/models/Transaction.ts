c9c1fac4d54da7f54b1028434b34e1df599509a3
Renato Hideki Osaka <renato.osaka@hotmail.com>
2020-11-04 09:29:51 -0300

    Revert "added realtion on model"
    This reverts commit 123661352d8ecb4bc9e0b0292841f18f5fcccf99.

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -4,11 +4,7 @@ import {
   Column,
   CreateDateColumn,
   UpdateDateColumn,
-  ManyToOne,
-  JoinColumn,
 } from 'typeorm';
-
-import Category from './Category';
 
 @Entity('transactions')
 class Transaction {
@@ -26,10 +22,6 @@ class Transaction {
 
   @Column()
   category_id: string;
-
-  @ManyToOne(() => Category)
-  @JoinColumn({ name: 'category_id' })
-  category: Category;
 
   @CreateDateColumn()
   created_at: Date;
