363158df7625090fcafdf7881c40d82f2d06c8c6
Renato Hideki Osaka <renato.osaka@hotmail.com>
2020-11-04 09:25:06 -0300

    created migrations

---
+++ src/database/migrations/1604492067366-CreateTransaction.ts
@@ -0,0 +1,63 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTransaction1604492067366
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'value',
+            type: 'decimal',
+            scale: 2,
+            precision: 10,
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'TransactionCategory',
+            columnNames: ['category_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'categories',
+            onDelete: 'SET NULL',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('transactions');
+  }
+}
